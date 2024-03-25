aa163e683f0aa921c0e7b6f3052279bbc5a54442
Faishal <faisal@tantowi.com>
2024-03-25 23:37:55 +0700

    remove alert

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 const App:React.FC = () => {
-  useEffect(() => {
-    alert('successfully deployed')
+  useEffect(() => { 
     window.location.href = "https://joyella-id.github.io/vidyaandfaishal/";
   }, []);
   return <div></div>;
